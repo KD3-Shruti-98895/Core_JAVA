dc49788859aa365187f9c6b4b4f5d0710670f9b0
Shruti.K <shrutidkadam02@gmail.com>
2026-09-22 15:38:58 +0530

    Classwork

---
+++ Day_6/src/com/subeam/Employee.java
@@ -0,0 +1,33 @@
+package com.subeam;
+
+public class Employee {
+	private String name;
+	private int age;
+	
+	public Employee( )
+	{
+		
+	}
+	public Employee(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public boolean equals(Object obj) {
+		Employee other = {Employee) obj; //downcasting
+	}
+
+}
---
+++ Day_6/src/com/subeam/Program.java
@@ -0,0 +1,28 @@
+package com.subeam;
+
+class Fruit {
+	private String type;
+	Fruit( ) { 
+		
+	}
+}
+
+class Apple extends Fruit {
+	private String color;
+	private double weight;
+	
+}
+
+class Orange extends Fruit {
+	
+}
+
+class Mango extends Fruit {
+	
+}
+
+public class Program {
+	public static void main(String [] args) {
+		
+	}
+}
